037fd260eedf1810b8d044bd6d7860b458d53987
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-11-21 20:01:59 +0330

    add main with basic handlers

---
+++ main.go
@@ -0,0 +1,18 @@
+package main 
+
+import ( 
+	"net/http"
+	"github.com/mehrdad3301/ChitChat/handler"
+) 
+
+
+func main() { 
+
+	fileHandler := http.FileServer(http.Dir("./public"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileHandler))
+	http.HandleFunc("/", handler.Home) 
+	http.HandleFunc("/login", handler.Login) 
+
+	http.ListenAndServe("localhost:6060", nil)
+}
+
