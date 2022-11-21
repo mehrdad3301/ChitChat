250458e7393e1c653158309d2f0160e9755068ed
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-11-21 22:56:10 +0330

    move .db .sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 
 create table users (
-  id         int primary key,
+  id         integer primary key autoincrement,
   name       varchar(255),
   email      varchar(255) not null unique,
   password   varchar(255) not null,
@@ -8,7 +8,7 @@ create table users (
 );
 
 create table sessions (
-  id         int primary key,
+  id         integer primary key autoincrement,
   uuid       varchar(64) not null unique,
   email      varchar(255),
   user_id    int references users(id),
@@ -16,14 +16,14 @@ create table sessions (
 );
 
 create table threads (
-  id         int primary key,
+  id         integer primary key autoincrement,
   topic      text,
   user_id    int references users(id),
   created_at text not null 
 );
 
 create table posts (
-  id         int primary key,
+  id         integer primary key autoincrement,
   body       text,
   user_id    int references users(id),
   thread_id  int references threads(id),
