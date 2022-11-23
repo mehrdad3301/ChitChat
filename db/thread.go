5e6a7c426a45557da5301f854cd9026611044337
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-11-23 09:49:59 +0330

    add thread.go in db package

--- db/thread.go
+++ db/thread.go
@@ -3,10 +3,11 @@ package db
 import ( 
 	"fmt"
 	"time"
+	"strconv"
 ) 
 
 
-func CreateThread(topic, userId string) (error) {
+func CreateThread(topic string, userId int) (error) {
 
 	queryString := `
 	insert into 	
@@ -21,3 +22,69 @@ func CreateThread(topic, userId string) (error) {
 
 	return nil
 }
+
+func (thread *Thread) NumReplies() (int) {
+
+	queryString := `
+	select 
+		count(*) 
+	from 
+		posts
+	where thread_id = ?`
+
+	rows, err := db.Query(queryString, thread.Id)
+	if err != nil {
+		fmt.Println("NumReplies: ", err)
+		return 0
+	}
+
+	var count int 
+
+	defer rows.Close()
+	if rows.Next() {
+		if err = rows.Scan(&count); err != nil {
+			fmt.Println("NumReplies: ", err)
+			return 0
+		}
+	}
+	return count 
+}
+
+
+func GetThreads() ([]Thread, error) {
+	query := `
+	select 
+		* 
+	from 
+		threads 
+	order by created_at DESC`
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("Threads: ", err)
+	}
+
+	threads := []Thread{}
+	
+	defer rows.Close()
+	for rows.Next() {
+		conv := Thread{}
+		err = rows.Scan(&conv.Id, &conv.Topic,
+						&conv.UserId, &conv.CreatedAt) 
+		if err != nil {
+			return nil, fmt.Errorf("Threads: ", err)
+		}
+		threads = append(threads, conv)
+	}
+	return threads, nil
+}
+
+func (t Thread) User() (*User) { 
+
+	user, err := GetUser("id", strconv.Itoa(t.UserId))
+	if err != nil { 
+		fmt.Println("User: ", err)
+	} 
+	return user 
+}
+
