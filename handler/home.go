2645f1276dfa5804658bb958edb1baea13da5457
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-11-23 09:51:55 +0330

    add threads in home

--- handler/home.go
+++ handler/home.go
@@ -5,6 +5,7 @@ import (
 
 	"fmt"
 	"net/http"
+	"github.com/mehrdad3301/ChitChat/db"
 )
 
 func Home( 
@@ -12,18 +13,22 @@ func Home(
 	r *http.Request, 
 ) { 
 	
-	//reading threads
-	//reading session if exists 
 	ok, err := checkSession(r) 
 	if err != nil { 
 		fmt.Println("Home: ", err)
 	}
+	//reading threads
+	threads, err := db.GetThreads() 
+	if err != nil { 
+		fmt.Println("Home: ", err)
+	}
+	//reading session if exists 
 	//executing templates 
 	
 	if ok { 
-		generateHTML(w, new(interface{}), "layout", "private.navbar", "index")
+		generateHTML(w, threads, "layout", "private.navbar", "index")
 	} else {	
-		generateHTML(w, new(interface{}), "layout", "public.navbar", "index")
+		generateHTML(w, threads, "layout", "public.navbar", "index")
 	}
 
 }
