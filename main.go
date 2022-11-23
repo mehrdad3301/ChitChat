9784d0e41ead1d27485a355e11cc08c66ba1c14c
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-11-23 14:26:52 +0330

    separating read from post

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	http.HandleFunc("/signup", handler.SignUp) 
 	http.HandleFunc("/thread/create", handler.CreateThread) 
 	http.HandleFunc("/thread/read", handler.ReadThread) 
+	http.HandleFunc("/thread/post", handler.PostThread) 
 	
 	http.ListenAndServe("localhost:6060", nil)
 }
